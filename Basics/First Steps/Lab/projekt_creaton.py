17c8f7505102f74d552ff5fa11918180f531210b
AsparuhZ <144880601+AsparuhZ@users.noreply.github.com>
2024-01-10 17:39:22 +0100

    Add files via upload

---
+++ Basics/First Steps/Lab/projekt_creaton.py
@@ -0,0 +1,5 @@
+name = input()
+number_projekts = int(input())
+time_need = number_projekts * 3
+result = f"The architect {name} will need {time_need} hours to complete {number_projekts} project/s. "
+print(result)
